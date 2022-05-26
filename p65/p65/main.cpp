a3772fd7ea5bbd6a76f5ac85c8b4f160237d8de2
simonleedotcom <102502721+simonleedotcom@users.noreply.github.com>
2022-05-26 17:25:31 +0900

    220526_p65
    class C++
    p.65 example

---
+++ p65/p65/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double width;
+	double height;
+
+	cout << "Width & Height : ";
+	cin >> width >> height;
+
+	cout << "Result : " << width * height << '\n';
+
+	return 0;
+}
